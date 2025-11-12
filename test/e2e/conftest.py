272cf10210fd1aa23a836e63045d72dfa54092d7
danila nikitin <danila.n2015@yandex.ru>
2025-11-12 17:02:21 +0300

    add test create user request

---
+++ test/e2e/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from starlette.testclient import TestClient
+
+from app.main import app
+from app.models.models import PrimaryToken
+from db.connection import create_session_factory
+from db.session_manager import SessionManager
+
+
+@pytest.fixture(scope="module")
+def session_factory(settings) -> async_sessionmaker[AsyncSession]:
+    return create_session_factory(settings)
+
+
+@pytest_asyncio.fixture(scope="module")
+async def session_manager(session_factory) -> SessionManager:
+    return SessionManager(session_factory)
+
+
+@pytest.fixture(scope="module")
+def client(settings) -> TestClient:
+    return TestClient(
+        app,
+        base_url="http://test",
+)
+
+
+@pytest.fixture(scope="module")
+def primary_token_str() -> str:
+    return "f4uib483yr4894894"
+
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def clean_db_before_test(session_manager: SessionManager, primary_token_str: str):
+    async with session_manager.start_with_commit() as open_session_manager:
+        await open_session_manager.users.delete_all()
+        await open_session_manager.primary_tokens.delete_all()
+
+    async with session_manager.start_with_commit() as open_session_manager:
+        primary_token = PrimaryToken(name="test", token=primary_token_str)
+        obj = await open_session_manager.primary_tokens.save(primary_token)
+        assert obj is not None
+
+    yield
--- test/session_manager/conftest.py
+++ test/session_manager/conftest.py
@@ -1,16 +1,28 @@
 from typing import Type
 
 import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
+from db.connection import create_session_factory
 from db.session_manager import SessionManager
-from test.session_manager.mock import SessionFactoryMock
+from settings.settings import Settings
 
-
-@pytest.fixture(scope="module")
-def test_session_factory() -> Type[SessionFactoryMock]:
-    return SessionFactoryMock
 
 
 @pytest.fixture(scope="module")
-def session_manager(test_session_factory) -> SessionManager:
-    return SessionManager(session_factory=test_session_factory)
+def session_factory(settings: Settings) -> async_sessionmaker[AsyncSession]:
+    return create_session_factory(settings)
+
+
+@pytest.fixture(scope="module")
+def session_manager(session_factory: async_sessionmaker[AsyncSession]) -> SessionManager:
+    return SessionManager(session_factory)
+
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def clean_db_before(session_manager: SessionManager):
+        async with session_manager.start_with_commit() as start:
+            await start.users.delete_all()
+
+        yield
